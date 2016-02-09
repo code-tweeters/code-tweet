6406eef5d3182ca52557e3a5993a5533a68d8cbd
vagrant <vagrant@precise32.(none)>
2016-02-09 00:42:25 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeTweet::Application.config.secret_key_base = '8e9156581159bb2ffdd25ca76bb96b609041e79b6c7546ad00da5a9ea78eff7e69989c4dc823f333b3180ae1a6153abf0819dc247978af64d753e66607a7b68b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeTweet::Application.config.session_store :cookie_store, key: '_code-tweet_session'
