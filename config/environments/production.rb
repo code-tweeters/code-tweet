615b1d42c6902420836fc39b6b0192dd73fb0e36
Erin Anthony <erinmadisonanthony@gmail.com>
2016-03-19 22:06:34 +0000

    enable users to sign up, sign in, sign out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 CodeTweet::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #Configure mailer
+  config.action_mailer.default_url_options = { host: 'code-tweet.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
